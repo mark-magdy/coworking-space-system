5bbd2ff00b88e4cf3b7868cc6b05d4aaa269c89b
Mark Magdy <mark.anis12@gmail.com>
2025-04-24 22:44:04 +0200

    update gitignore

--- coworking-space.BAL/obj/Debug/net8.0/coworking-space.BAL.AssemblyInfo.cs
+++ coworking-space.BAL/obj/Debug/net8.0/coworking-space.BAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("coworking-space.BAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd85e350de60f31bbafd2fda4cbf4a23f0e63ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e4bb8ceac10ed16c6cd2db1364a993031a18f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("coworking-space.BAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("coworking-space.BAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
