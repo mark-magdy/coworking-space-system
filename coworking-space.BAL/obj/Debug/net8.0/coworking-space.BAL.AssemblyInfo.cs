bd3ac7f7e3989361a117d519774f6a523cc14000
jessyfady <jessyfady@icloud.com>
2025-05-14 16:04:48 +0300

    abl jessy

--- coworking-space.BAL/obj/Debug/net8.0/coworking-space.BAL.AssemblyInfo.cs
+++ coworking-space.BAL/obj/Debug/net8.0/coworking-space.BAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("coworking-space.BAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6bcdf3659e82d5c53fcb1120975cc7c9516b8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06b9d3c1e2fae9858f39874f09de6c8cbf2229eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("coworking-space.BAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("coworking-space.BAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
